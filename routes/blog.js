841f20fa58f4dfe8c2b44980163aef2c724cab14
Vishnu Vardhan Vankayalapati <v_vankalaya@uncg.edu>
2024-08-17 03:38:43 +0530

    update button

--- routes/blog.js
+++ routes/blog.js
@@ -66,16 +66,49 @@ router.get("/posts/:id", async function (req, res) {
     return res.status(404).render("404");
   }
 
-  post.humanReadableDate = post.date.toLocaleDateString('en-US', {
-    weekday: 'long',
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric'
+  post.humanReadableDate = post.date.toLocaleDateString("en-US", {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
   });
 
   post.date = post.date.toISOString();
 
   res.render("post-detail", { post: post });
+});
+
+router.get("/posts/:id/edit", async function (req, res) {
+  const postId = req.params.id;
+  const post = await db
+    .getDb()
+    .collection("posts")
+    .findOne({ _id: new ObjectId(postId) }, { title: 1, summary: 1, body: 1 });
+
+  if (!post) {
+    return res.status(404).render("404");
+  }
+
+  res.render("update-post", { post: post });
+});
+
+router.get("/posts/:id/edit", async function (req, res) {
+  const postId = new ObjectId(req.params.id);
+  const result = await db.getDb()
+    .collection("posts")
+    .updateOne(
+      { _id: postId },
+      {
+        $set: {
+          title: req.body.title,
+          summary: req.body.summary,
+          body: req.body.content,
+          date: new Date()
+        },
+      }
+    );
+
+    res.redirect('/posts');
 });
 
 module.exports = router;
