31ae878cffbff2dd705a496c95daf1d06de27f7d
Vishnu Vardhan Vankayalapati <v_vankalaya@uncg.edu>
2024-08-17 02:59:31 +0530

    fetching a single document

--- routes/blog.js
+++ routes/blog.js
@@ -21,7 +21,7 @@ router.get("/posts", async function (req, res) {
       {
         title: 1,
         summary: 1,
-        'author.name' : 1
+        "author.name": 1,
       }
     )
     .toArray();
@@ -53,6 +53,20 @@ router.post("/posts", async function (req, res) {
   const result = await db.getDb().collection("posts").insertOne(newPost);
   console.log(result);
   res.redirect("/posts");
+});
+
+router.get("/posts/:id", async function (req, res) {
+  const postId = req.params.id;
+  const post = await db
+    .getDb()
+    .collection("posts")
+    .findOne({ _id: new ObjectId(postId) }, { summary: 0 });
+
+  if (!post) {
+    return res.status(404).render("404");
+  }
+
+  res.render("post-detail", { post: post });
 });
 
 module.exports = router;
