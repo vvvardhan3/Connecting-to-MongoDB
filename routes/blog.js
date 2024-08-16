ce846740538913324bde0707834bb9edb93bd8c2
Vishnu Vardhan Vankayalapati <v_vankalaya@uncg.edu>
2024-08-17 02:40:11 +0530

    fetching & displaying the documents

--- routes/blog.js
+++ routes/blog.js
@@ -1,31 +1,44 @@
-const express = require('express');
-const mongodb = require('mongodb');
+const express = require("express");
+const mongodb = require("mongodb");
 
-const db = require('../data/database');
-const { name } = require('ejs');
+const db = require("../data/database");
+const { name } = require("ejs");
 
 const ObjectId = mongodb.ObjectId;
 
 const router = express.Router();
 
-router.get('/', function(req, res) {
-  res.redirect('/posts');
+router.get("/", function (req, res) {
+  res.redirect("/posts");
 });
 
-router.get('/posts', function(req, res) {
-  res.render('posts-list');
+router.get("/posts", async function (req, res) {
+  const posts = await db
+    .getDb()
+    .collection("posts")
+    .find(
+      {},
+      {
+        title: 1,
+        summary: 1,
+        'author.name' : 1
+      }
+    )
+    .toArray();
+  res.render("posts-list", { posts: posts });
 });
 
-router.get('/new-post', async function(req, res) {
-
-  const authors= await db.getDb().collection('authors').find().toArray();
-  res.render('create-post', {authors: authors});
+router.get("/new-post", async function (req, res) {
+  const authors = await db.getDb().collection("authors").find().toArray();
+  res.render("create-post", { authors: authors });
 });
 
-
-router.post('/posts', async function (req, res) {
-  const authorId =  new ObjectId(req.body.author);
-  const author = await db.getDb().collection('authors').findOne( { _id: authorId } )
+router.post("/posts", async function (req, res) {
+  const authorId = new ObjectId(req.body.author);
+  const author = await db
+    .getDb()
+    .collection("authors")
+    .findOne({ _id: authorId });
   const newPost = {
     title: req.body.title,
     summary: req.body.summary,
@@ -33,14 +46,13 @@ router.post('/posts', async function (req, res) {
     date: new Date(),
     authors: {
       id: authorId,
-      name: author.name
+      name: author.name,
+    },
+  };
 
-    }
-  }
-
-  const result = await db.getDb().collection('posts').insertOne(newPost);
+  const result = await db.getDb().collection("posts").insertOne(newPost);
   console.log(result);
-  res.redirect('/posts');
+  res.redirect("/posts");
 });
 
 module.exports = router;
